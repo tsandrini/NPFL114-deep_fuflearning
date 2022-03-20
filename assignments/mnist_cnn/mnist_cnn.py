897d2f5ec8d62b25e4973895f7eb62ce85268592
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2022-03-20 15:57:37 +0100

    Completed mnist_cnn assignment

--- assignments/mnist_cnn/mnist_cnn.py
+++ assignments/mnist_cnn/mnist_cnn.py
@@ -4,6 +4,7 @@
 import argparse
 import datetime
 import os
+from platform import architecture
 import re
 from typing import Dict
 
@@ -29,12 +30,154 @@ parser.add_argument(
 # If you add more arguments, ReCodEx will keep them with your default values.
 
 
+def _parse_layer_name_and_spec(layer_string):
+    if "-" in layer_string:
+        layer_name, layer_spec = layer_string.split("-", 1)
+    else:
+        layer_name, layer_spec = layer_string, None
+    return layer_name, layer_spec
+
+
+def _get_layer_dict(layer):
+    layer_name, layer_spec = _parse_layer_name_and_spec(layer)
+    layer_dict = {"type": layer_name, "orig_spec": layer_spec}
+
+    if layer_name == "C":
+
+        def _build_conv2d(layer_dict, layer_input):
+            return tf.keras.layers.Conv2D(*layer_dict["args"], **layer_dict["kwargs"])(
+                layer_input
+            )
+
+        filters, kernel_size, strides, padding = layer_spec.split("-")
+
+        layer_dict["build_fn"] = _build_conv2d
+        layer_dict["args"] = [int(filters), int(kernel_size)]
+        layer_dict["kwargs"] = {
+            "strides": int(strides),
+            "padding": padding,
+            "activation": "relu",
+        }
+
+    elif layer_name == "CB":
+
+        def _build_conv2d_batchnorm(layer_dict, layer_input):
+            h1 = tf.keras.layers.Conv2D(*layer_dict["args"], **layer_dict["kwargs"])(
+                layer_input
+            )
+            h2 = tf.keras.layers.BatchNormalization()(h1)
+            return tf.keras.layers.Activation("relu")(h2)
+
+        filters, kernel_size, strides, padding = layer_spec.split("-")
+
+        layer_dict["build_fn"] = _build_conv2d_batchnorm
+        layer_dict["args"] = [int(filters), int(kernel_size)]
+        layer_dict["kwargs"] = {
+            "strides": int(strides),
+            "padding": padding,
+            "activation": None,
+            "use_bias": False,
+        }
+
+    elif layer_name == "M":  # Max pooling 2D
+
+        def _build_max_pooling(layer_dict, layer_input):
+            return tf.keras.layers.MaxPooling2D(
+                *layer_dict["args"], **layer_dict["kwargs"]
+            )(layer_input)
+
+        pool_size, strides = layer_spec.split("-")
+
+        layer_dict["build_fn"] = _build_max_pooling
+        layer_dict["args"] = []
+        layer_dict["kwargs"] = {"pool_size": int(pool_size), "strides": int(strides)}
+
+    elif layer_name == "R":
+
+        def _build_residual_block(layer_dict, layer_input):
+            x = layer_input
+            for sublayer_dict in layer_dict["layers"]:
+                x = sublayer_dict["build_fn"](sublayer_dict, x)
+
+            return tf.keras.layers.Add()([layer_input, x])
+
+        layer_dict["build_fn"] = _build_residual_block
+        layer_dict["args"] = []
+        layer_dict["kwargs"] = {}
+        layer_dict["layers"] = [
+            _get_layer_dict(spec) for spec in layer_spec[1:-1].split(",")
+        ]
+
+    elif layer_name == "F":  # Flatten layer
+
+        def _build_flatten(layer_dict, layer_input):
+            return tf.keras.layers.Flatten(*layer_dict["args"], **layer_dict["kwargs"])(
+                layer_input
+            )
+
+        layer_dict["build_fn"] = _build_flatten
+        layer_dict["args"] = []
+        layer_dict["kwargs"] = {}
+
+    elif layer_name == "H":  # Dense layer
+
+        def _build_dense(layer_dict, layer_input):
+            return tf.keras.layers.Dense(*layer_dict["args"], **layer_dict["kwargs"])(
+                layer_input
+            )
+
+        layer_dict["build_fn"] = _build_dense
+        layer_dict["args"] = [int(layer_spec)]
+        layer_dict["kwargs"] = {"activation": "relu"}
+
+    elif layer_name == "D":
+
+        def _build_dropout(layer_dict, layer_input):
+            return tf.keras.layers.Dropout(*layer_dict["args"], **layer_dict["kwargs"])(
+                layer_input
+            )
+
+        layer_dict["build_fn"] = _build_dropout
+        layer_dict["args"] = [float(layer_spec)]
+        layer_dict["kwargs"] = {}
+
+    return layer_dict
+
+
+def _build_nn_spec(layers_str: str):
+    arch_raw = list()
+    arch_string = layers_str
+    while True:
+        if "," not in arch_string:
+            arch_raw.append(arch_string)
+            break
+
+        left, right = arch_string.split(",", 1)
+        if "[" in left:
+            left, right = arch_string.split("],", 1)
+            arch_raw.append(left + "]")
+        else:
+            arch_raw.append(left)
+
+        arch_string = right
+
+    architecture = list()
+    for layer in arch_raw:
+        architecture.append(_get_layer_dict(layer))
+    return architecture
+
+
 # The neural network model
 class Model(tf.keras.Model):
     def __init__(self, args: argparse.Namespace) -> None:
         # TODO: Create the model. The template uses the functional API, but
         # feel free to use subclassing if you want.
         inputs = tf.keras.layers.Input(shape=[MNIST.H, MNIST.W, MNIST.C])
+
+        layer_dicts = _build_nn_spec(args.cnn)
+        hidden = inputs
+        for layer_dict in layer_dicts:
+            hidden = layer_dict["build_fn"](layer_dict, hidden)
 
         # TODO: Add CNN layers specified by `args.cnn`, which contains
         # a comma-separated list of the following layers:
@@ -56,7 +199,7 @@ class Model(tf.keras.Model):
         # You can assume the resulting network is valid; it is fine to crash if it is not.
         #
         # Produce the results in the variable `hidden`.
-        hidden = ...
+        # hidden = ...
 
         # Add the final output layer
         outputs = tf.keras.layers.Dense(MNIST.LABELS, activation=tf.nn.softmax)(hidden)
