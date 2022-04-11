36a5cbd631b1ea2a7b6d73edb0dc38fa7e743703
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2022-04-11 17:36:01 +0200

    Completed tagger_we assignment

--- assignments/tagger_we/tagger_we.py
+++ assignments/tagger_we/tagger_we.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+# 53907afe-531b-11ea-a595-00505601122b
+# b7ea974c-d389-11e8-a4be-00505601122b
 import argparse
 import datetime
 import os
 import re
 from typing import Dict
+
 os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
 
 import numpy as np
@@ -15,14 +18,28 @@ parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
 parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
-parser.add_argument("--max_sentences", default=None, type=int, help="Maximum number of sentences to load.")
-parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument(
+    "--max_sentences",
+    default=None,
+    type=int,
+    help="Maximum number of sentences to load.",
+)
+parser.add_argument(
+    "--recodex", default=False, action="store_true", help="Evaluation in ReCodEx."
+)
 parser.add_argument("--rnn_cell", default="LSTM", type=str, help="RNN cell type.")
 parser.add_argument("--rnn_cell_dim", default=64, type=int, help="RNN cell dimension.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument(
+    "--threads", default=1, type=int, help="Maximum number of threads to use."
+)
 parser.add_argument("--we_dim", default=128, type=int, help="Word embedding dimension.")
 # If you add more arguments, ReCodEx will keep them with your default values.
+
+LAYERS = {
+    "LSTM": tf.keras.layers.LSTM,
+    "GRU": tf.keras.layers.GRU,
+}
 
 
 class Model(tf.keras.Model):
@@ -32,26 +49,44 @@ class Model(tf.keras.Model):
         words = tf.keras.layers.Input(shape=[None], dtype=tf.string, ragged=True)
 
         # TODO: Map strings in `words` to indices by using the `word_mapping` of `train.forms`.
+        indices = train.forms.word_mapping(words)
 
         # TODO: Embed input words with dimensionality `args.we_dim`. Note that the `word_mapping`
         # provides a `vocabulary_size()` call returning the number of unique words in the mapping.
+        embedding = tf.keras.layers.Embedding(
+            train.forms.word_mapping.vocabulary_size(), args.we_dim
+        )(indices)
 
         # TODO: Create the specified `args.rnn_cell` RNN cell (LSTM, GRU) with
         # dimension `args.rnn_cell_dim`. The cell should produce an output for every
         # sequence element (so a 3D output). Then apply it in a bidirectional way on
         # the embedded words, **summing** the outputs of forward and backward RNNs.
+        sequences = tf.keras.layers.Bidirectional(
+            LAYERS[args.rnn_cell](
+                args.rnn_cell_dim,
+                return_sequences=True,
+                return_state=False,
+            ),
+            merge_mode="sum",
+        )(embedding)
 
         # TODO: Add a softmax classification layer into as many classes as there are unique
         # tags in the `word_mapping` of `train.tags`. Note that the Dense layer can process
         # a `RaggedTensor` without any problem.
-        predictions = None
+        predictions = tf.keras.layers.Dense(
+            train.tags.word_mapping.vocabulary_size(), activation="softmax"
+        )(sequences)
 
         # Check that the created predictions are a 3D tensor.
         assert predictions.shape.rank == 3
         super().__init__(inputs=words, outputs=predictions)
-        self.compile(optimizer=tf.optimizers.Adam(),
-                     loss=lambda yt, yp: tf.losses.SparseCategoricalCrossentropy()(yt.values, yp.values),
-                     metrics=[tf.metrics.SparseCategoricalAccuracy(name="accuracy")])
+        self.compile(
+            optimizer=tf.optimizers.Adam(),
+            loss=lambda yt, yp: tf.losses.SparseCategoricalCrossentropy()(
+                yt.values, yp.values
+            ),
+            metrics=[tf.metrics.SparseCategoricalAccuracy(name="accuracy")],
+        )
 
         self.tb_callback = tf.keras.callbacks.TensorBoard(args.logdir)
 
@@ -63,11 +98,19 @@ def main(args: argparse.Namespace) -> Dict[str, float]:
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
     # Create logdir name
-    args.logdir = os.path.join("logs", "{}-{}-{}".format(
-        os.path.basename(globals().get("__file__", "notebook")),
-        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v) for k, v in sorted(vars(args).items())))
-    ))
+    args.logdir = os.path.join(
+        "logs",
+        "{}-{}-{}".format(
+            os.path.basename(globals().get("__file__", "notebook")),
+            datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+            ",".join(
+                (
+                    "{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v)
+                    for k, v in sorted(vars(args).items())
+                )
+            ),
+        ),
+    )
 
     # Load the data
     morpho = MorphoDataset("czech_cac", max_sentences=args.max_sentences)
@@ -80,20 +123,33 @@ def main(args: argparse.Namespace) -> Dict[str, float]:
     # - a tensor of integral tag ids as targets.
     # To create the tag ids, use the `word_mapping` of `morpho.train.tags`.
     def extract_tagging_data(example):
-        raise NotImplementedError()
+        return (example["forms"], morpho.train.tags.word_mapping(example["tags"]))
 
     def create_dataset(name):
         dataset = getattr(morpho, name).dataset
         dataset = dataset.map(extract_tagging_data)
-        dataset = dataset.shuffle(len(dataset), seed=args.seed) if name == "train" else dataset
-        dataset = dataset.apply(tf.data.experimental.dense_to_ragged_batch(args.batch_size))
+        dataset = (
+            dataset.shuffle(len(dataset), seed=args.seed)
+            if name == "train"
+            else dataset
+        )
+        dataset = dataset.apply(
+            tf.data.experimental.dense_to_ragged_batch(args.batch_size)
+        )
         return dataset
+
     train, dev = create_dataset("train"), create_dataset("dev")
 
-    logs = model.fit(train, epochs=args.epochs, validation_data=dev, callbacks=[model.tb_callback])
+    logs = model.fit(
+        train, epochs=args.epochs, validation_data=dev, callbacks=[model.tb_callback]
+    )
 
     # Return development metrics for ReCodEx to validate
-    return {metric: values[-1] for metric, values in logs.history.items() if metric.startswith("val_")}
+    return {
+        metric: values[-1]
+        for metric, values in logs.history.items()
+        if metric.startswith("val_")
+    }
 
 
 if __name__ == "__main__":
