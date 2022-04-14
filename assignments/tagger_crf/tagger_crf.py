c3f891118fd9c545edf51dff3121307a1ff22dab
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2022-04-14 11:46:30 +0200

    Completed tagger_crf assignment

--- assignments/tagger_crf/tagger_crf.py
+++ assignments/tagger_crf/tagger_crf.py
@@ -6,6 +6,7 @@ import datetime
 import os
 import re
 from typing import Dict
+
 os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
 
 import numpy as np
@@ -18,14 +19,28 @@ parser = argparse.ArgumentParser()
 # These arguments will be set appropriately by ReCodEx, even if you change them.
 parser.add_argument("--batch_size", default=10, type=int, help="Batch size.")
 parser.add_argument("--epochs", default=5, type=int, help="Number of epochs.")
-parser.add_argument("--max_sentences", default=None, type=int, help="Maximum number of sentences to load.")
-parser.add_argument("--recodex", default=False, action="store_true", help="Evaluation in ReCodEx.")
+parser.add_argument(
+    "--max_sentences",
+    default=None,
+    type=int,
+    help="Maximum number of sentences to load.",
+)
+parser.add_argument(
+    "--recodex", default=False, action="store_true", help="Evaluation in ReCodEx."
+)
 parser.add_argument("--rnn_cell", default="LSTM", type=str, help="RNN cell type.")
 parser.add_argument("--rnn_cell_dim", default=64, type=int, help="RNN cell dimension.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument(
+    "--threads", default=1, type=int, help="Maximum number of threads to use."
+)
 parser.add_argument("--we_dim", default=128, type=int, help="Word embedding dimension.")
 # If you add more arguments, ReCodEx will keep them with your default values.
+
+LAYERS = {
+    "LSTM": tf.keras.layers.LSTM,
+    "GRU": tf.keras.layers.GRU,
+}
 
 
 class Model(tf.keras.Model):
@@ -35,38 +50,73 @@ class Model(tf.keras.Model):
         words = tf.keras.layers.Input(shape=[None], dtype=tf.string, ragged=True)
 
         # TODO(tagger_we): Map strings in `words` to indices by using the `word_mapping` of `train.forms`.
+        indices = train.forms.word_mapping(words)
 
         # TODO(tagger_we): Embed input words with dimensionality `args.we_dim`. Note that the `word_mapping`
         # provides a `vocabulary_size()` call returning the number of unique words in the mapping.
+        embedding = tf.keras.layers.Embedding(
+            train.forms.word_mapping.vocabulary_size(), args.we_dim
+        )(indices)
 
         # TODO(tagger_we): Create the specified `args.rnn_cell` RNN cell (LSTM, GRU) with
         # dimension `args.rnn_cell_dim`. The cell should produce an output for every
         # sequence element (so a 3D output). Then apply it in a bidirectional way on
         # the embedded words, **summing** the outputs of forward and backward RNNs.
+        sequences = tf.keras.layers.Bidirectional(
+            LAYERS[args.rnn_cell](
+                args.rnn_cell_dim,
+                return_sequences=True,
+                return_state=False,
+            ),
+            merge_mode="sum",
+        )(embedding)
 
         # TODO: Add a final classification layer into as many classes as there are unique
         # tags in the `word_mapping` of `train.tags`. Note that **no activation** should
         # be used, the CRF operations will take care of it.
-        predictions = None
+        predictions = tf.keras.layers.Dense(
+            train.tags.word_mapping.vocabulary_size(), activation=None
+        )(sequences)
 
         # Check that the created predictions are a 3D tensor.
         assert predictions.shape.rank == 3
         super().__init__(inputs=words, outputs=predictions)
 
         # We compile the model with CRF loss and SpanLabelingF1 metric.
-        self.compile(optimizer=tf.optimizers.Adam(),
-                     loss=self.crf_loss,
-                     metrics=[self.SpanLabelingF1Metric(train.tags.word_mapping.get_vocabulary(), name="f1")])
+        self.compile(
+            optimizer=tf.optimizers.Adam(),
+            loss=self.crf_loss,
+            metrics=[
+                self.SpanLabelingF1Metric(
+                    train.tags.word_mapping.get_vocabulary(), name="f1"
+                )
+            ],
+        )
 
         # TODO: Create `self._crf_weights`, a trainable zero-initialized tf.float32 matrix variable
         # of size [number of unique train tags, number of unique train tags], using `self.add_weight`.
-        self._crf_weights = self.add_weight(...)
+        self._crf_weights = self.add_weight(
+            name="crf_weights",
+            shape=[
+                train.tags.word_mapping.vocabulary_size(),
+                train.tags.word_mapping.vocabulary_size(),
+            ],
+            dtype=tf.float32,
+            trainable=True,
+            initializer=tf.keras.initializers.Zeros(),
+        )
 
         self.tb_callback = tf.keras.callbacks.TensorBoard(args.logdir)
 
-    def crf_loss(self, gold_labels: tf.RaggedTensor, logits: tf.RaggedTensor) -> tf.Tensor:
-        assert isinstance(gold_labels, tf.RaggedTensor), "Gold labels given to CRF loss must be RaggedTensors"
-        assert isinstance(logits, tf.RaggedTensor), "Logits given to CRF loss must be RaggedTensors"
+    def crf_loss(
+        self, gold_labels: tf.RaggedTensor, logits: tf.RaggedTensor
+    ) -> tf.Tensor:
+        assert isinstance(
+            gold_labels, tf.RaggedTensor
+        ), "Gold labels given to CRF loss must be RaggedTensors"
+        assert isinstance(
+            logits, tf.RaggedTensor
+        ), "Logits given to CRF loss must be RaggedTensors"
 
         # TODO: Use `tfa.text.crf_log_likelihood` to compute the CRF log likelihood.
         # You will have to convert both logits and gold_labels to dense Tensors and
@@ -75,15 +125,33 @@ class Model(tf.keras.Model):
         #
         # Finally, compute the loss using the computed log likelihoods, averaging the
         # individual batch examples.
-        raise NotImplementedError()
+
+        # print(
+        #     gold_labels.dtype, gold_labels.row_lengths(), gold_labels.to_tensor().shape
+        # )
+        # print(logits.dtype, logits.row_lengths(), logits.to_tensor().shape)
+        crf, _ = tfa.text.crf_log_likelihood(
+            inputs=logits.to_tensor(),
+            tag_indices=gold_labels.to_tensor(),
+            sequence_lengths=gold_labels.row_lengths(),
+            transition_params=self._crf_weights,
+        )
+        return tf.reduce_mean(-crf, axis=0)
 
     def crf_decode(self, logits: tf.RaggedTensor) -> tf.RaggedTensor:
-        assert isinstance(logits, tf.RaggedTensor), "Logits given to CRF decoding must be RaggedTensors"
+        assert isinstance(
+            logits, tf.RaggedTensor
+        ), "Logits given to CRF decoding must be RaggedTensors"
 
         # TODO: Perform CRF decoding using `tfa.text.crf_decode`. Convert the
         # logits analogously as in `crf_loss`. Finally, convert the result
         # to a ragged tensor.
-        predictions = ...
+        preds, _ = tfa.text.crf_decode(
+            logits.to_tensor(),
+            transition_params=self._crf_weights,
+            sequence_length=logits.row_lengths(),
+        )
+        predictions = tf.RaggedTensor.from_tensor(preds, lengths=logits.row_lengths())
 
         assert isinstance(predictions, tf.RaggedTensor)
         return predictions
@@ -96,7 +164,9 @@ class Model(tf.keras.Model):
             y_pred = self(x, training=True)
             loss = self.compute_loss(x, y, y_pred)
         self.optimizer.minimize(loss, self.trainable_variables, tape=tape)
-        return {"loss": metric.result() for metric in self.metrics if metric.name == "loss"}
+        return {
+            "loss": metric.result() for metric in self.metrics if metric.name == "loss"
+        }
 
     # We override `predict_step` to run CRF decoding during prediction.
     def predict_step(self, data):
@@ -115,10 +185,13 @@ class Model(tf.keras.Model):
 
     class SpanLabelingF1Metric(tf.metrics.Metric):
         """Keras-like metric evaluating span labeling F1-score of RaggedTensors."""
+
         def __init__(self, tags, name="span_labeling_f1", dtype=None):
             super().__init__(name, dtype)
             self._tags = tags
-            self._counts = self.add_weight("counts", shape=[3], initializer=tf.initializers.Zeros(), dtype=tf.int64)
+            self._counts = self.add_weight(
+                "counts", shape=[3], initializer=tf.initializers.Zeros(), dtype=tf.int64
+            )
 
         def reset_state(self):
             self._counts.assign([0] * 3)
@@ -136,18 +209,30 @@ class Model(tf.keras.Model):
                         span, start = label[2:], i
                 if span:
                     spans.add((start, len(labels), span))
-            return np.array([len(spans_true & spans_pred), len(spans_pred - spans_true),
-                             len(spans_true - spans_pred)], np.int64)
+            return np.array(
+                [
+                    len(spans_true & spans_pred),
+                    len(spans_pred - spans_true),
+                    len(spans_true - spans_pred),
+                ],
+                np.int64,
+            )
 
         def update_state(self, y, y_pred, sample_weight=None):
-            assert isinstance(y, tf.RaggedTensor) and isinstance(y_pred, tf.RaggedTensor)
+            assert isinstance(y, tf.RaggedTensor) and isinstance(
+                y_pred, tf.RaggedTensor
+            )
             assert sample_weight is None, "sample_weight currently not supported"
-            counts = tf.numpy_function(self.classify_spans, (y.values, y_pred.values, y.row_limits()), tf.int64)
+            counts = tf.numpy_function(
+                self.classify_spans, (y.values, y_pred.values, y.row_limits()), tf.int64
+            )
             self._counts.assign_add(counts)
 
         def result(self):
             tp, fp, fn = self._counts[0], self._counts[1], self._counts[2]
-            return tf.math.divide_no_nan(tf.cast(2 * tp, tf.float32), tf.cast(2 * tp + fp + fn, tf.float32))
+            return tf.math.divide_no_nan(
+                tf.cast(2 * tp, tf.float32), tf.cast(2 * tp + fp + fn, tf.float32)
+            )
 
 
 def main(args: argparse.Namespace) -> Dict[str, float]:
@@ -157,11 +242,19 @@ def main(args: argparse.Namespace) -> Dict[str, float]:
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
     # Create logdir name
-    args.logdir = os.path.join("logs", "{}-{}-{}".format(
-        os.path.basename(globals().get("__file__", "notebook")),
-        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v) for k, v in sorted(vars(args).items())))
-    ))
+    args.logdir = os.path.join(
+        "logs",
+        "{}-{}-{}".format(
+            os.path.basename(globals().get("__file__", "notebook")),
+            datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+            ",".join(
+                (
+                    "{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v)
+                    for k, v in sorted(vars(args).items())
+                )
+            ),
+        ),
+    )
 
     # Load the data
     morpho = MorphoDataset("czech_cnec", max_sentences=args.max_sentences)
@@ -174,17 +267,26 @@ def main(args: argparse.Namespace) -> Dict[str, float]:
     # - a tensor of integral tag ids as targets.
     # To create the tag ids, use the `word_mapping` of `morpho.train.tags`.
     def extract_tagging_data(example):
-        raise NotImplementedError()
+        return (example["forms"], morpho.train.tags.word_mapping(example["tags"]))
 
     def create_dataset(name):
         dataset = getattr(morpho, name).dataset
         dataset = dataset.map(extract_tagging_data)
-        dataset = dataset.shuffle(len(dataset), seed=args.seed) if name == "train" else dataset
-        dataset = dataset.apply(tf.data.experimental.dense_to_ragged_batch(args.batch_size))
+        dataset = (
+            dataset.shuffle(len(dataset), seed=args.seed)
+            if name == "train"
+            else dataset
+        )
+        dataset = dataset.apply(
+            tf.data.experimental.dense_to_ragged_batch(args.batch_size)
+        )
         return dataset
+
     train, dev = create_dataset("train"), create_dataset("dev")
 
-    logs = model.fit(train, epochs=args.epochs, validation_data=dev, callbacks=[model.tb_callback])
+    logs = model.fit(
+        train, epochs=args.epochs, validation_data=dev, callbacks=[model.tb_callback]
+    )
 
     # Return all metrics for ReCodEx to validate
     return {metric: values[-1] for metric, values in logs.history.items()}
