be921d336251e54aea638a097d59a7cb2be83be7
Tomáš Sandrini <tomas.sandrini@seznam.cz>
2022-05-02 23:39:10 +0200

    Completed 3d_recognition assignment

--- assignments/3d_recognition/3d_recognition.py
+++ assignments/3d_recognition/3d_recognition.py
@@ -5,6 +5,7 @@ import argparse
 import datetime
 import os
 import re
+
 os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "2")  # Report only TF errors by default
 
 import numpy as np
@@ -19,7 +20,9 @@ parser.add_argument("--batch_size", default=None, type=int, help="Batch size.")
 parser.add_argument("--epochs", default=None, type=int, help="Number of epochs.")
 parser.add_argument("--modelnet", default=20, type=int, help="ModelNet dimension.")
 parser.add_argument("--seed", default=42, type=int, help="Random seed.")
-parser.add_argument("--threads", default=1, type=int, help="Maximum number of threads to use.")
+parser.add_argument(
+    "--threads", default=1, type=int, help="Maximum number of threads to use."
+)
 
 
 def main(args: argparse.Namespace) -> None:
@@ -29,21 +32,31 @@ def main(args: argparse.Namespace) -> None:
     tf.config.threading.set_intra_op_parallelism_threads(args.threads)
 
     # Create logdir name
-    args.logdir = os.path.join("logs", "{}-{}-{}".format(
-        os.path.basename(globals().get("__file__", "notebook")),
-        datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
-        ",".join(("{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v) for k, v in sorted(vars(args).items())))
-    ))
+    args.logdir = os.path.join(
+        "logs",
+        "{}-{}-{}".format(
+            os.path.basename(globals().get("__file__", "notebook")),
+            datetime.datetime.now().strftime("%Y-%m-%d_%H%M%S"),
+            ",".join(
+                (
+                    "{}={}".format(re.sub("(.)[^_]*_?", r"\1", k), v)
+                    for k, v in sorted(vars(args).items())
+                )
+            ),
+        ),
+    )
 
     # Load the data
     modelnet = ModelNet(args.modelnet)
 
     # TODO: Create the model and train it
-    model = ...
+    # model = ...
 
     # Generate test set annotations, but in `args.logdir` to allow parallel execution.
     os.makedirs(args.logdir, exist_ok=True)
-    with open(os.path.join(args.logdir, "3d_recognition.txt"), "w", encoding="utf-8") as predictions_file:
+    with open(
+        os.path.join(args.logdir, "3d_recognition.txt"), "w", encoding="utf-8"
+    ) as predictions_file:
         # TODO: Predict the probabilities on the test set
         test_probabilities = model.predict(...)
 
